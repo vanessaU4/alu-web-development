452a60404ff424140dfbb7bdc0e6ba2bbaeb2ec6
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2025-01-16 09:24:59 +0000

    fifo

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,24 +1,43 @@
-#!/usr/bin/env python3
-"""fifo cache"""
-BaseCaching = __import__('base_caching').BaseCaching
+#!/usr/bin/python3
+""" FIFO caching """
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFO Cache class"""
+    """ Class that inherits from BaseCaching and is a caching system """
+
+    def __init__(self):
+        super().__init__()
+        self.data = {}
+        self.next_in, self.next_out = 0, 0
+
+    def _pop(self):
+        """ FIFO algorithm, remove element """
+        self.next_out += 1
+        key = self.data[self.next_out]
+        del self.data[self.next_out], self.cache_data[key]
+
+    def _push(self, key, item):
+        """ FIFO algorithm, add element """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.data[self.next_out + 1]))
+            self._pop()
+        self.cache_data[key] = item
+        self.next_in += 1
+        self.data[self.next_in] = key
 
     def put(self, key, item):
-        """Add an item in the cache"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discard = next(iter(self.cache_data))
-            print("DISCARD: {}".format(discard))
-            del self.cache_data[discard]
-        self.cache_data[key] = item
+        """ Assign to the dictionary """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                self._push(key, item)
 
     def get(self, key):
-        """Get an item by key"""
-        if key is None or key not in self.cache_data:
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
             return None
-        return self.cache_data[key]
-        
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return value
